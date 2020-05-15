8b0c8f005962dbf01c4848257e2e376c3d30cf6f
bababumBab <antonica.gabriel@gmail.com>
2020-05-15 20:54:53 +0100

    Updates

---
+++ precache-manifest.2254aec7b68a9994439786325d7144b1.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a6d2aa2e23df8859b2cef9550f81f04",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2e7f21b3c850aa11c774",
+    "url": "/static/css/main.c518bf70.chunk.css"
+  },
+  {
+    "revision": "bf87bb617e0b94350e73",
+    "url": "/static/js/2.688ea8a7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.688ea8a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e7f21b3c850aa11c774",
+    "url": "/static/js/main.309fbe90.chunk.js"
+  },
+  {
+    "revision": "eb57f27a3734ab91271c",
+    "url": "/static/js/runtime-main.deeacdfd.js"
+  },
+  {
+    "revision": "fbbf3818d1782acbf82dde51c52412b7",
+    "url": "/static/media/Headshot_bw_pattern.fbbf3818.png"
+  },
+  {
+    "revision": "fbbf3818d1782acbf82dde51c52412b7",
+    "url": "/static/media/Headshot_c_pattern.fbbf3818.png"
+  },
+  {
+    "revision": "9945c64ad58109471fdf685cedf822db",
+    "url": "/static/media/Image_1.9945c64a.png"
+  },
+  {
+    "revision": "43001c09f79237579b8771242cf62c2f",
+    "url": "/static/media/Image_1_fo.43001c09.png"
+  },
+  {
+    "revision": "de96f99989ada67fed47cd31aabbe957",
+    "url": "/static/media/Image_1_fr.de96f999.png"
+  },
+  {
+    "revision": "b67cf1154fba0d68bce0bb635437dc31",
+    "url": "/static/media/Image_1_fu.b67cf115.png"
+  },
+  {
+    "revision": "726c473045801873a6085cc87dfad585",
+    "url": "/static/media/Mask_Group_1_d_pattern.726c4730.png"
+  },
+  {
+    "revision": "726c473045801873a6085cc87dfad585",
+    "url": "/static/media/Mask_Group_1_e_pattern.726c4730.png"
+  },
+  {
+    "revision": "c51578203d61c7ffccad8d9dbcfc3226",
+    "url": "/static/media/heroImage2x.c5157820.png"
+  }
+]);
